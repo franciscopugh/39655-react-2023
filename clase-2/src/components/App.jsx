412acb19a923b8ee79d0025c02ae78ae78d1fef2
Francisco Pugh <franciscopugh01@gmail.com>
2023-03-25 11:58:50 -0300

    Clase 2 A

---
+++ clase-2/src/components/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+export const App = () => {
+  /*
+    HTML      JSX
+    class -> className
+    <input> -> <input />
+    `${valor}` -> {valor}
+    style = "nombreProp: valor" -> style= {{"nombreProp": "valor"}}
+    mayor parte de las propiedades -> camelCase
+  */
+  const valor = 5
+  return (
+    <div className='divGeneral' style={{ backgroundColor: "red" }}>
+      <h1>Hola!</h1>
+      <p>{valor}</p>
+      <input type="text" />
+    </div>
+  )
+}
